f58270ff9208cbf0a70cf271d614dfbac22d9679
sarmisthapal10 <73027162+sarmisthapal10@users.noreply.github.com>
2022-10-30 23:05:58 +0530

    Initial Commit

---
+++ HelloWorldClass.java
@@ -0,0 +1,9 @@
+class HelloWorldClass
+{
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String args[])
+    {
+        System.out.println("Hello World from This Class");
+    }
+}
